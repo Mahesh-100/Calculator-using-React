04311510d9c048bba27f812e64ed3f794125f6fc
Mahesh-100 <maheshkumar.ucen@gmail.com>
2023-12-11 23:47:20 +0530

    Calculater

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+import "./style.css";
 const App=()=>{
-    return <h1>React demo</h1>
+    return (
+        <div className="Calculator-grid">
+            <div className="Output">
+                <div className="previous-operand"></div>
+                <div className="current-operand"></div>
+            </div>
+            <button className="span-two">AC</button>
+            <button>DEL</button>
+            <button>÷</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>*</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>-</button>
+            <button>.</button>
+            <button>0</button>
+            <button className="span-two">=</button>
+
+        </div>
+    )
+
+    
 }
 ReactDOM.render(<App/>,document.getElementById("root"))
