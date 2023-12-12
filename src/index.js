4d016f2de46859fe0ae1282f24efeb13d70bd82d
Mahesh-100 <maheshkumar.ucen@gmail.com>
2023-12-12 19:38:24 +0530

    Calculator in progress

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
-import React from "react";
+import React  from "react";
 import  ReactDOM  from "react-dom";
-import "./style.css";
-const App=()=>{
-    return (
-        <div className="Calculator-grid">
-            <div className="Output">
-                <div className="previous-operand"></div>
-                <div className="current-operand"></div>
-            </div>
-            <button className="span-two">AC</button>
-            <button>DEL</button>
-            <button>÷</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
-            <button className="span-two">=</button>
-
-        </div>
-    )
-
-    
+import App from "./App";
+const Calculator=()=>{
+    return <App/>
 }
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.render(<Calculator/>,document.getElementById("root"))
